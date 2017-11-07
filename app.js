8012bd8da4fcb24e3292305759190b561addf025
Vanessa Jiménez <vanessajimenez@vanessa-jimenezs-macbook-2.local>
2017-11-07 13:04:42 -0500

    Working on lodash

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
 const express = require ('express');
 const app = express();
 const volleyball = require('volleyball');
+const nunjucks = require('nunjucks');
+
+
+
+var locals = {
+title : 'An Example',
+people : [
+{name : 'Gandalf'},
+{name : 'Frodo'},
+{name : 'Hermione'}
+]
+
+}
+
+nunjucks.configure('views',{ noCache: true })
+
 
 
 app.use(volleyball);
 
 app.get('/', (req, res) => {
-  res.send("Welcome");
+  nunjucks.render('index.html',locals,
+  function(err,renderedContent) {res.send(renderedContent)}
+)
+;
 });
+
 
 
 
